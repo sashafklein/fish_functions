10c02f4871f8ac59a1469069ffb89f2f2bae7dc5
Sasha Klein <sashafklein@gmail.com>
2014-02-19 11:38:05 -0800

    Whitespace stuff and improved code method

--- config.fish
+++ config.fish
@@ -1,102 +1,120 @@
-rvm > /dev/null # load RVM environment
-set PATH /Applications/Postgres.app/Contents/MacOS/bin $PATH
-set fish_greeting ""
-set PATH bin $PATH # for bundle binstubs
-set PATH /usr/local/bin $PATH
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Theme
+set fish_theme robbyrussell
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins autojump bundler
+
+# Path to your custom folder (default path is $FISH/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+
+rvm > /dev/null
+
+
 
 ##############################
 ######### ZEUS BASED #########
 ##############################
 
 function z
-	zeus start
+  zeus start
 end
 
 function rr
-	if zeus_on
-		if [ (count $argv) -gt 0 ]
-			zeus rake routes | grep $argv 
-		else
-			zeus rake routes
-		end
-	else
-		echo "Zeus is not running"
-		if [ (count $argv) -gt 0 ]
-			rake routes | grep $argv
-		else
-			rake routes
-		end
-	end
+  if zeus_on
+    if [ (count $argv) -gt 0 ]
+      zeus rake routes | grep $argv 
+    else
+      zeus rake routes
+    end
+  else
+    echo "Zeus is not running"
+    if [ (count $argv) -gt 0 ]
+      rake routes | grep $argv
+    else
+      rake routes
+    end
+  end
 end
 
 function zeus_on
-	if ps aux | grep -v grep | grep 'zeus slave: development_environment'
-		true
-	else
-		false
-	end
+  if ps aux | grep -v grep | grep 'zeus slave: development_environment'
+    true
+  else
+    false
+  end
 end
 
 function mg
-	if zeus_on
-		zeus rake db:migrate $argv
-	else
-		echo "Zeus is not running"
-		rake db:migrate $argv
-	end
+  if zeus_on
+    zeus rake db:migrate $argv
+  else
+    echo "Zeus is not running"
+    rake db:migrate $argv
+  end
 end
 
 function tprep
-	if zeus_on
-		zeus rake db:test:prepare $argv
-	else
-		echo "Zeus is not running"
-		rake db:test:prepare $argv
-	end
+  if zeus_on
+    zeus rake db:test:prepare $argv
+  else
+    echo "Zeus is not running"
+    rake db:test:prepare $argv
+  end
 end
 
 function s 
-	if zeus_on
-		zeus s
-	else
-		echo "Zeus is not running"
-		rails s
-	end
+  if zeus_on
+    zeus s
+  else
+    echo "Zeus is not running"
+    rails s
+  end
 end
 
 function t
-	if zeus_on
-		if [ (count $argv) -gt 0 ]
-			zeus test $argv
-		else
-			zeus test spec
-		end
-	else
-		echo "Zeus is not running"
-		if [ (count $argv) -gt 0 ]
-			rspec $argv
-		else
-			rspec spec
-		end
-	end
+  if zeus_on
+    if [ (count $argv) -gt 0 ]
+      zeus test $argv
+    else
+      zeus test spec
+    end
+  else
+    echo "Zeus is not running"
+    if [ (count $argv) -gt 0 ]
+      rspec $argv
+    else
+      rspec spec
+    end
+  end
+end
+
+function tt
+  zeus rspec --tag $argv spec
 end
 
 function r
-	if zeus_on
-		zeus rake $argv
-	else
-		echo "Zeus is not running"
-		rake $argv
-	end
+  if zeus_on
+    zeus rake $argv
+  else
+    echo "Zeus is not running"
+    rake $argv
+  end
 end
 
 function c
-	if zeus_on
-		zeus c $argv
-	else
-		echo "Zeus is not running"
-		rails c $argv
-	end
+  if zeus_on
+    zeus c $argv
+  else
+    echo "Zeus is not running"
+    rails c $argv
+  end
 end
 
 ##############################
@@ -104,7 +122,7 @@ end
 ##############################
 
 function gd
-	git diff $argv
+  git diff $argv
 end
 
 function gds
@@ -128,64 +146,64 @@ function gh
 end
 
 function gac
-	ga
-	gc $argv
+  ga
+  gc $argv
 end
 
 function gacm
-	gac -m $argv
+  gac -m $argv
 end
 
 function ga
-	if [ (count $argv) -gt 0 ]
-		git add $argv
-	else
-		git add .
-	end
+  if [ (count $argv) -gt 0 ]
+    git add $argv
+  else
+    git add .
+  end
 end
 
 function gp
-	git pull $argv
+  git pull $argv
 end
 
 function gs
-	git status $argv
+  git status $argv
 end
 
 function gsp
-	git stash pop
+  git stash pop
 end
 
 function gss
-	git stash save
+  git stash save
 end
 
 function gl
-	git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --
+  git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --
 end
 
 function gco
-	git checkout $argv
+  git checkout $argv
 end
 
 function gcom
-	git checkout master
+  git checkout master
 end
 
 function gpush
-	git push $argv
+  git push $argv
 end
 
 function gb
-	git branch $argv
+  git branch $argv
 end
 
 function gbd
-	git branch -d $argv
+  git branch -d $argv
 end
 
 function gc
-	git commit $argv
+  git commit $argv
 end
 
 function grb
@@ -193,11 +211,11 @@ function grb
 end
 
 function g
-	git $argv
+  git $argv
 end
 
 function gcpick
-	git cherry-pick $argv
+  git cherry-pick $argv
 end
 
 ###############################
@@ -205,21 +223,29 @@ end
 ###############################
 
 function fish_dir
-	cd ~/.config/fish/
+  cd ~/.config/fish/
 end
 
 function xf
-	cd ~/rails/ctw2
+  cd ~/rails/ctw2
 end
 
 # Requires a variable $code_directory set to directory name
 # eg: set -U code_directory "rails"
 function code
-	cd ~/$code_directory
+  if [ (count $argv) -gt 0 ]
+    cd ~/$code_directory/$argv
+  else
+    cd ~/$code_directory
+  end
+end
+
+function design
+  cd ~/$code_directory/design-roadmap
 end
 
 function bloc
-	cd ~/code/bloc
+  cd ~/code/bloc
 end
 
 function meatup
@@ -227,11 +253,11 @@ function meatup
 end
 
 function wod
-	cd ~/rails/ctw2
+  cd ~/rails/ctw2
 end
 
 function course
-	cd ~/rails/bitstarter
+  cd ~/rails/bitstarter
 end
 
 function fit
@@ -242,12 +268,16 @@ end
 ########### OTHER ############
 ##############################
 
+function fj
+  functional-javascript $argv
+end
+
 function ll
-	ls -lh $argv
+  ls -lh $argv
 end
 
 function fs
-	foreman start $argv
+  foreman start $argv
 end
 
 function hcon
@@ -255,7 +285,7 @@ function hcon
 end
 
 function dtest
-	tail -f diagnostic.txt
+  tail -f diagnostic.txt
 end
 
 function sb
@@ -263,16 +293,16 @@ function sb
 
     switch $result
         case '*sashafklein*'
-        	if [ (count $argv) -gt 0 ]
-            	sublime $argv
+          if [ (count $argv) -gt 0 ]
+              sublime $argv
             else
-            	sublime .
+              sublime .
             end
         case '*alexanderfklein*'
-       		if [ (count $argv) -gt 0 ]
-            	sub $argv
+          if [ (count $argv) -gt 0 ]
+              sub $argv
             else
-            	sub .
+              sub .
             end
         case '*'
             echo $result
